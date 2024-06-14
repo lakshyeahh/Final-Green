6354499ae8cb5f8e59b46f69d63833c243b0d656
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-14 19:46:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,44 @@
-import http from 'http'
-import app from './app.js'
+import express from 'express';
+import path from 'path'
+const app = express();
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import router from './routes/index.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors'
 
-const PORT = process.env.PORT || 4000;
 
 
-const server = http.createServer(app);
-server.listen(PORT,()=>{console.log('this app is running on '+PORT)});
+dotenv.config();
+app.use(cors(
+  {
+      origin: ["https://deploy-mern-frontend.vercel.app"],
+      methods: ["POST", "GET", "PUT", "PATCH", 'DELETE'],
+      credentials: true
+  }
+));
+app.use(express.json())
+
+const PORT = process.env.PORT || 4000
+
+
+
+//routes
+app.get("/", (req, res) => {
+  res.json("Hello");
+})
+app.use('/api', router)
+
+
+mongoose.connect(mongodb+srv://lakshya:lakshya@cluster0.nqysbfw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(PORT, () => {
+      console.log('listening for requests on port', PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
