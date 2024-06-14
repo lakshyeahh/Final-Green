77308d82df1dfd47766d014d8d67e2edcf32a049
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-14 13:43:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,31 @@
 import express from 'express';
-import path from 'path'
-const app = express();
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import router from './routes/index.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors'
-
-
+import cors from 'cors';
 
 dotenv.config();
-app.use(cors(
-  {
-      origin: ["https://deploy-mern-frontend.vercel.app"],
-      methods: ["POST", "GET", "PUT", "PATCH", 'DELETE'],
-      credentials: true
-  }
-));
-app.use(express.json())
 
-const PORT = process.env.PORT || 4000
+const app = express();
+const PORT = process.env.PORT || 4000;
 
+app.use(cors({
+  origin: ["https://deploy-mern-frontend.vercel.app"],
+  methods: ["POST", "GET", "PUT", "PATCH", 'DELETE'],
+  credentials: true
+}));
 
+app.use(express.json());
 
-//routes
-app.get("/", (req, res) => {
-  res.json("Hello");
-})
-app.use('/api', router)
-
+app.use('/api', router);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('connected to database')
-    // listen to port
+    console.log('Connected to database');
     app.listen(PORT, () => {
-      console.log('listening for requests on port', PORT)
-    })
+      console.log('Listening for requests on port', PORT);
+    });
   })
   .catch((err) => {
-    console.log(err)
-  }) 
+    console.error('Error connecting to database:', err);
+  });
