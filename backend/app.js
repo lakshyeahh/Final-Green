4b455b21d7c98f0d3361114c16f72b808b2a98d4
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-14 14:17:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,23 @@
+import express from 'express';
+const app = express();
+import router from './routes/index.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import router from './routes/index.js';
 
+dotenv.config();
+
+app.use(express.json())
+
+app.use(cors());
+
+app.get("*", (req, res,next) => {
+  res.status(200).json({
+    message: "Hello"
+  })
+})
+
+app.use('/api', router)
+
+export default app;
