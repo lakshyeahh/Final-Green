64f4667ba4e943d01b92f48a59294f918a0b4ce6
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-14 13:44:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,12 @@ import express from 'express';
 import router from './routes/index.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({
-  origin: ["https://deploy-mern-frontend.vercel.app"],
-  methods: ["POST", "GET", "PUT", "PATCH", 'DELETE'],
-  credentials: true
-}));
 
 app.use(express.json());
 
