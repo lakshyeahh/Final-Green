ef549f5e700b05fadb1a19dd9f17c33033105651
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-14 14:21:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,8 @@
-import mongoose from 'mongoose';
 const http = require('http');
 const app = require('./app');
 
 const PORT = process.env.PORT || 4000;
 
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Connected to database');
-    app.listen(PORT, () => {
-      console.log('Listening for requests on port', PORT);
-    });
-  })
-  .catch((err) => {
-    console.error('Error connecting to database:', err);
-  });
+const server = http.createServer(app);
+server.listen(port,()=>{console.log('this app is running on '+port)});
