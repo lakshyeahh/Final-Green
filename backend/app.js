4c05f85229e89797aed3332c63cc29785ae7c60b
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-14 14:20:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,14 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes/index.js';
 
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connection.on('error',err=>{
+  console.log('connection failed');
+});
+mongoose.connection.on('connected',()=>{
+  console.log('connected successfully with database');
+});
+
 dotenv.config();
 
 app.use(express.json())
