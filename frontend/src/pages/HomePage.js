52c3520f9ced6a119c351e324cbb7d4c3a26f913
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-15 10:18:16 +0530

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -24,7 +24,7 @@ function HomePage() {
           throw new Error('Access token not found');
         }
 
-        const response = await fetch('/api/users', {
+        const response = await fetch(`${process.env.REACT_APP_URL}/api/users`, {
           method: 'GET',
           headers: {
             'Authorization': `Bearer ${token}`
