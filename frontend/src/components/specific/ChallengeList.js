fca01ca3a29cd46b272f06888afde4ed10feed93
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-15 13:24:21 +0530

    Update ChallengeList.js

--- frontend/src/components/specific/ChallengeList.js
+++ frontend/src/components/specific/ChallengeList.js
@@ -17,7 +17,7 @@ function ChallengeList() {
 
     const fetchChallenges = async () => {
         try {
-            const response = await fetch(`${process.env.REACT_APP_URL}/api/challenges`);
+            const response = await fetch('https://final-green-api.vercel.app/api/challenges');
 
             if (!response.ok) {
                 throw new Error('Failed to fetch challenges data');
