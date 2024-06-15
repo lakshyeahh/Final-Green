481812e25f29d2d9b21fa22d0cc708b17e1d69a8
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-15 10:20:50 +0530

    Update dashboard.js

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -31,7 +31,7 @@ function Dashboard() {
           throw new Error('Access token not found');
         }
 
-        const response = await fetch('/api/users', {
+        const response = await fetch(`${process.env.REACT_APP_URL}/api/users`, {
           method: 'GET',
           headers: {
             'Authorization': `Bearer ${token}`
@@ -59,7 +59,7 @@ function Dashboard() {
           throw new Error('Access token not found');
         }
 
-      const response = await fetch('/api/logout', {
+      const response = await fetch(`${process.env.REACT_APP_URL}/api/logout`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
