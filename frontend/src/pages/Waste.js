cdb50256429e859b138ee184a1adef3904169b1a
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-15 10:20:17 +0530

    Update Waste.js

--- frontend/src/pages/Waste.js
+++ frontend/src/pages/Waste.js
@@ -25,7 +25,7 @@ function Waste() {
             throw new Error('Access token not found');
           }
   
-          const response = await fetch('/api/users', {
+          const response = await fetch(`${process.env.REACT_APP_URL}/api/users`, {
             method: 'GET',
             headers: {
               'Authorization': `Bearer ${token}`
