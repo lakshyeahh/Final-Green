c72b4eda1ce0e523515edd2c2c3aca819f911d8e
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2024-06-15 10:17:26 +0530

    Update Event.js

--- frontend/src/pages/Event.js
+++ frontend/src/pages/Event.js
@@ -31,7 +31,7 @@ function Challenge() {
           throw new Error('Access token not found');
         }
 
-        const response = await fetch('/api/users', {
+        const response = await fetch(`${process.env.REACT_APP_URL}/api/users`, {
           method: 'GET',
           headers: {
             'Authorization': `Bearer ${token}`
@@ -58,7 +58,7 @@ function Challenge() {
 
 
 
-        const response = await fetch('/api/events', {
+        const response = await fetch(`${process.env.REACT_APP_URL}/api/events`, {
           method: 'GET',
           headers: {
             'Content': `application/json`
